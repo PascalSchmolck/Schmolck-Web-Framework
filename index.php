514e6bb1bbb107fa99a62c9b3629dcef1e74c86e
Pascal Schmolck <piccolino81@gmail.com>
2013-01-09 23:20:12 +0100

    [Framework] Optimize class autoloader - implement dynamic class path parsing

--- index.php
+++ index.php
@@ -11,24 +11,18 @@
  * AUTOLOADER
  */
 function __autoload($strClass){
-	$arrPart = explode("_", strtolower($strClass));
-	switch(count($arrPart)){
-		case 1:
-			$file = "lib/php/{$arrPart[0]}/{$arrPart[0]}.php";
-			break;
-		case 2:
-			$file = "lib/php/{$arrPart[0]}/{$arrPart[1]}/{$arrPart[1]}.php";
-			break;
-		case 3:
-			$file = "lib/php/{$arrPart[0]}/{$arrPart[1]}/{$arrPart[2]}/{$arrPart[2]}.php";
-			break;
-		case 4:
-			$file = "lib/php/{$arrPart[0]}/{$arrPart[1]}/{$arrPart[2]}/{$arrPart[3]}/{$arrPart[3]}.php";
-			break;
+	// - prepare path
+	$strFilePath = 'lib/php';
+	
+	// - parse class parts
+	$arrParts = explode('_', $strClass);
+	foreach ($arrParts as $strPart) {
+		$strFilePath .= "/{$strPart}";
+		$strFileName = "{$strPart}.php";
 	}
-	if(isset($file)){
-		require_once($file);
-	}
+	
+	// - include
+	require_once($strFilePath.'/'.$strFileName);
 }
 
 /*
