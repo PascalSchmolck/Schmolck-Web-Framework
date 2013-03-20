9d834dd426d8c7be752ea6364c19281e72c41cad
Pascal Schmolck <piccolino81@gmail.com>
2013-03-20 01:23:24 +0100

    [CARS] implement gallery view for search results

--- application/cars.schmolck.de/module/cars/search/result/action.php
+++ application/cars.schmolck.de/module/cars/search/result/action.php
@@ -7,42 +7,15 @@
 $objCore = Schmolck_Framework_Core::getInstance($this);
 $objCore->strId = $objCore->getHelperApi()->getId();
 $objCore->strUri = $objCore->getHelperApplication()->getRequestUri();
-$objCore->strStyleClass = $objCore->getHelperApi()->getStyleClass();
-// - cars
-$objCars = new Schmolck_Cars_Helper($objCore);
-// - offset
-$objCore->nOffset = 10;
+$objCore->strApi = $objCore->getHelperApi()->getIdentifier();
 
 /*
  * PARAMETER
  */
-// - limit
-$objCore->nLimit = intval(strip_tags($_POST['limit']));
-if ($objCore->nLimit == 0) {
-	$objCore->nLimit = max(Schmolck_Tool_Memory::restore(get_class(), 'limit'), $objCore->nOffset);
-} else {
-	Schmolck_Tool_Memory::store(get_class(), 'limit', $objCore->nLimit);
+$objCore->strParameterMode = Schmolck_Tool_Memory::auto($objCore->strApi, 'mode', strip_tags($_POST['mode']));
+if ($objCore->strParameterMode == '') {
+	$objCore->strParameterMode = 'list';
 }
-
-/*
- * DATA
- */
-//$strQuery = sprintf("SELECT * FROM  `mod_cars` LIMIT 0,%s", $objCore->nLimit);
-//$resource = $objCore->getHelperDatabase()->query($strQuery);
-//while ($arrRow = mysql_fetch_assoc($resource)) {
-//	$arrRow['name'] = $objCars->extractName($arrRow);
-//	$arrRow["EZ"] = $objCars->extractEz($arrRow);
-//	$arrRow["KM"] = $objCars->extractKm($arrRow);
-//	$arrRow["RP"] = $objCars->extractPrice($arrRow);
-//	$arrRow["color"] = $objCars->extractColor($arrRow);
-//	$arrRow["image"] = $objCars->extractFirstImageUrl($arrRow);
-//	$arrResult[] = $arrRow;
-//}
-//$objCore->arrCars = $arrResult;
-//$objCore->nCars = count($objCore->arrCars);
-
-$objCore->arrCars = $objCars->queryFilteredCars();
-$objCore->nCars = count($objCore->arrCars);
 
 /*
  * SCRIPT
--- application/cars.schmolck.de/module/cars/search/result/scripts.js
+++ application/cars.schmolck.de/module/cars/search/result/scripts.js
@@ -2,47 +2,24 @@ $(document).ready(function() {
 	//alert(window.location.hash);
 	
 	/*
-	 * SCROLLING
-	 */
-	// - scroll to previous position
-	var hash = window.location.hash.replace('#', '');
-	if (hash != '') {
-		$('html, body').stop().animate({
-			scrollTop: window.location.hash.replace('#', '')
-		}, 500);
-		window.location.hash = '';
-	}
-	
-	/*
 	 * LINKS
 	 */
-	$('#SchmolckID .item').each(function() {
+	$('#SchmolckID .links a').each(function() {
 		// - action
 		$(this).click(function() {
-			// - save scroll position
-			window.location.hash = $(window).scrollTop();			
-			// - open link
-			window.location.href = $(this).data('link');
+			SchmolckID_send($(this).data('mode'));
+			return false;
 		});
-	});
-	
-	/*
-	* ACTION
-	*/
-	$('#SchmolckID .loadlink .button').click(function() {
-		SchmolckID_send();
-		return false;
 	});
 		
 	/*
 	* AJAX
 	*/
-	SchmolckID_send = function() {
-		var strData = $('#SchmolckID form').serialize();
+	SchmolckID_send = function(strMode) {
 		Schmolck_Framework_Helper_Api({
 			url: 'SchmolckURI',
 			id: 'SchmolckID',
-			data: strData
+			data: 'mode='+strMode
 		});
 	}
 
