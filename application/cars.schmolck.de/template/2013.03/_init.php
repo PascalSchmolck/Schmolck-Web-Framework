273aefa29cc8f0532e87963c4d0b16eb526d39d9
Pascal Schmolck <piccolino81@gmail.com>
2013-03-29 23:11:42 +0100

    [FRAMEWORK] include CSSgrid styles into framework styles

--- application/cars.schmolck.de/template/2013.03/_init.php
+++ application/cars.schmolck.de/template/2013.03/_init.php
@@ -6,7 +6,6 @@
 $objCore = Schmolck_Framework_Core::getInstance($this);
 $strTemplatePath = $objCore->getHelperApplication()->getTemplatePath();
 // - foreign scripts
-//$objCore->registerLayoutScript('lib/less/less-1.3.0.min.js');
 $objCore->registerLayoutScript('lib/jquery/jquery-1.9.1.min.js');
 //$objCore->registerLayoutScript('lib/jquery/ui/jquery-ui-1.10.1.custom.min.js');
 // - framework scripts
@@ -16,6 +15,7 @@ $objCore->registerLayoutScript('lib/schmolck/framework/helper/link/link.js');
 $objCore->registerLayoutScript($strTemplatePath . '/scripts.js');
 
 // - layout styles
+$objCore->registerLayoutStyle('lib/cssgrid/1140.css');
 //$objCore->registerLayoutStyle('lib/jquery/ui/ui-lightness/jquery-ui-1.10.1.custom.min.css');
 
 /*
