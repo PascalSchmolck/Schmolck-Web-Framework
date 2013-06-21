51d2a2c8ec842f9599e8ba708c44930176ce1ef3
Pascal Schmolck <mail@pascalschmolck.de>
2013-06-22 00:24:06 +0200

    [LAYOUT] remove scrollup image button due to Windows Phone issues

--- application/cars.schmolck.de/template/2013.05/scripts.js
+++ application/cars.schmolck.de/template/2013.05/scripts.js
@@ -12,19 +12,4 @@ $(document).ready(function() {
 	 */
 	$('.slideDown').slideDown('slow');
 	
-	/*
-	 * SCROLLUP
-	 */
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollup').fadeIn();
-		} else {
-			$('.scrollup').fadeOut();
-		}
-	}); 
-	$('.scrollup').click(function(){
-		$("html, body").animate({ scrollTop: 0 }, 600);
-		return false;
-	});
-
 });
