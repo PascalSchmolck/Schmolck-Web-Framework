900a7032780714b0c452d5ce470feb56613f9f14
Pascal Schmolck <mail@pascalschmolck.de>
2013-07-22 13:38:41 +0200

    [MOBILE] re-arrange contact persons according to request

---
+++ application/cars.schmolck.de/module/cars/contact/sidebar/action.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * INITIALISATION
+ */
+// - core
+$objCore = Schmolck_Framework_Core::getInstance($this);
+$objCore->strId = $objCore->getHelperApi()->getId();
+$objCore->strUrl = $objCore->getHelperApplication()->getRequestUrl();
+$objCore->strApi = $objCore->getHelperApi()->getIdentifier();
+
+/*
+ * SCRIPT
+ */
+$objCore->getHelperScripts()->registerViewScriptReplace(array(
+	'SchmolckID' => $objCore->strId,
+	'SchmolckURL' => $objCore->strUrl,
+	'SchmolckRESULTID' => $objCore->strParameterResultID,
+));
