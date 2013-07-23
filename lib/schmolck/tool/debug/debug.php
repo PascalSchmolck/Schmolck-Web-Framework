dce52e8061a712b9e3a6675aa3765a2ab8204c0a
Pascal Schmolck <mail@pascalschmolck.de>
2013-07-23 16:48:37 +0200

    [FRAMEWORK] improve debugging by adding handling for arrays

--- lib/schmolck/tool/debug/debug.php
+++ lib/schmolck/tool/debug/debug.php
@@ -12,84 +12,94 @@ class Schmolck_Tool_Debug {
 	/**
 	 * Log alert message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function emergency($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('EMERGENCY', self::_getFullMessage($strMessage, $strFile, $strLine), 0);
+	static public function emergency($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('EMERGENCY', self::_getFullMessage($objMessage, $strFile, $strLine), 0);
 	}
 
 	/**
 	 * Log alert message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function alert($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('ALERT', self::_getFullMessage($strMessage, $strFile, $strLine), 1);
+	static public function alert($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('ALERT', self::_getFullMessage($objMessage, $strFile, $strLine), 1);
 	}
 
 	/**
 	 * Log critical message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function critical($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('CRITICAL', self::_getFullMessage($strMessage, $strFile, $strLine), 2);
+	static public function critical($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('CRITICAL', self::_getFullMessage($objMessage, $strFile, $strLine), 2);
 	}
 
 	/**
 	 * Log error message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function error($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('ERROR', self::_getFullMessage($strMessage, $strFile, $strLine), 3);
+	static public function error($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('ERROR', self::_getFullMessage($objMessage, $strFile, $strLine), 3);
 	}
 
 	/**
 	 * Log warning message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function warning($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('WARNING', self::_getFullMessage($strMessage, $strFile, $strLine), 4);
+	static public function warning($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('WARNING', self::_getFullMessage($objMessage, $strFile, $strLine), 4);
 	}
 
 	/**
 	 * Log notice message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function notice($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('NOTICE', self::_getFullMessage($strMessage, $strFile, $strLine), 5);
+	static public function notice($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('NOTICE', self::_getFullMessage($objMessage, $strFile, $strLine), 5);
 	}
 
 	/**
 	 * Log info message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function info($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('INFO', self::_getFullMessage($strMessage, $strFile, $strLine), 6);
+	static public function info($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('INFO', self::_getFullMessage($objMessage, $strFile, $strLine), 6);
 	}
 
 	/**
 	 * Log debug message
 	 *
-	 * @param string $strMessage log message
+	 * @param object $objMessaage log message
 	 */
-	static public function debug($strMessage, $strFile='', $strLine='') {
-		self::_WriteMessage('DEBUG', self::_getFullMessage($strMessage, $strFile, $strLine), 7);
+	static public function debug($objMessage, $strFile='', $strLine='') {
+		self::_WriteMessage('DEBUG', self::_getFullMessage($objMessage, $strFile, $strLine), 7);
 	}
 
 	/**
 	 * Get full message containing file, line and message nicely formatted
 	 * 
-	 * @param type $strMessage
-	 * @param type $strFile
-	 * @param type $strLine
+	 * @param object $objMessaage log message
+	 * @param string $strFile
+	 * @param string $strLine
 	 * @return string full message
 	 */
-	static private function _getFullMessage($strMessage, $strFile='?', $strLine='?') {
+	static private function _getFullMessage($objMessage, $strFile='?', $strLine='?') {
+		switch(gettype($objMessage)) {
+			default:
+				$strMessage = $objMessage;
+				break;
+			case 'array':
+			case 'object':
+				$strMessage = print_r($objMessage, true);
+				break;
+		}
+		
 		if ($strFile == '' and $strLine == '') {
 			return $strMessage;
 		} else {
@@ -110,11 +120,11 @@ class Schmolck_Tool_Debug {
 	 * 7 Debug: debug messages
 	 *
 	 * @param string $strType message type
-	 * @param string $strMessage message text
+	 * @param string $objMessage message text
 	 * @param int $nLevel number of backtrace steps
 	 *
 	 */
-	static private function _WriteMessage($strType, $strMessage, $nLevel) {
+	static private function _WriteMessage($strType, $objMessage, $nLevel) {
 		//-------
 		// CHECK
 		//-------
@@ -126,7 +136,7 @@ class Schmolck_Tool_Debug {
 		/*
 		 * MESSAGE
 		 */
-		$strLogMessage .= "{$strType}: {$strMessage}";
+		$strLogMessage .= "{$strType}: {$objMessage}";
 
 		/*
 		 * OUTPUT
