82fe0e8867547f677b9fb91cee0265d7dc4dbe36
Pascal Schmolck <mail@pascalschmolck.de>
2013-07-26 14:20:38 +0200

    [x] [MOBILE] merge configuration from localhost to production server file

--- host/cars.schmolck.de.php
+++ host/cars.schmolck.de.php
@@ -107,3 +107,19 @@ define('CARS_RECIPIENT_ADDRESS', 'info@schmolck.de');
 //define('CARS_LOCATION_SYNCFILE', 'http://www.schmolck.de/data/private/vehicles/IFZ.csv');
 define('CARS_LOCATION_IMAGES', 'data/cars/images/sync');
 define('CARS_LOCATION_SYNCFILE', 'http://cars.schmolck.de/data/cars/files/sync/IFZ.csv');
+
+/*
+ * MOBILE
+ */
+// - database & import & images
+define('MOBILE_ZIP_FILE', 'data/mobile/files/sync/test.zip');
+define('MOBILE_CSV_FILE_NAME', 'media.csv');
+define('MOBILE_CSV_DELIMITER', ';');
+define('MOBILE_CSV_ENCLOSURE', '"');
+define('MOBILE_CSV_LIMITS', '171,5,16');
+define('MOBILE_IMAGES_PATH', 'data/mobile/images/sync');
+define('MOBILE_DATABASE_FILE', 'data/mobile/files/sync/media.csv');
+define('MOBILE_DATABASE_TABLE', 'mod_mobile_claris_20130724');
+// - contact form
+define('MOBILE_RECIPIENT_NAME', 'cars.schmolck.de');
+define('MOBILE_RECIPIENT_ADDRESS', 'info@schmolck.de');
