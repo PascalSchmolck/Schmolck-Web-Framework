298a3d4110577a1fbceea6d3a646df98ba0713ae
Pascal Schmolck <mail@pascalschmolck.de>
2015-03-18 00:26:10 +0100

    [x] [PROJECT] add url redirect if http://www.schmolck.de/cars has been called [x] [MOBILE] add url redirect if wrong www.cars... url has been called

---
+++ application/cars.schmolck.de/module/_init.php
@@ -0,0 +1,12 @@
+<?php
+
+// INITIALISATION
+$objCore = Schmolck_Framework_Core::getInstance($this);
+$objCore->strId = $objCore->getHelperApi()->getId();
+
+// Redirect...
+if ($objCore->getHelperApplication()->getBaseUrl() == 'http://www.cars.schmolck.de') { 
+    // ...if www.cars instead of cars.schmolck.de has been called
+    $objCore->getHelperRedirect()->external('http://cars.schmolck.de');
+    exit();
+}
--- application/www.schmolck.de/module/index/index/index/action.php
+++ application/www.schmolck.de/module/index/index/index/action.php
@@ -1,12 +1,8 @@
 <?php
 
-/*
- * INITIALISATION
- */
+// INITIALISATION
 $objCore = Schmolck_Framework_Core::getInstance($this);
 $objCore->strId = $objCore->getHelperApi()->getId();
 
-/*
- * REDIRECT
- */
-$objCore->getHelperRedirect()->external('http://news.schmolck.de');
+// REDIRECT
+//$objCore->getHelperRedirect()->external('http://news.schmolck.de');
