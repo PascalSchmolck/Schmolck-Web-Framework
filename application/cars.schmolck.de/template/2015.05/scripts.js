2a017ae3eb8b9c693759406eb0c6fa108ab9099f
Pascal Schmolck <mail@pascalschmolck.de>
2015-05-21 23:02:33 +0200

    Continue bootstrap template 2015.05
    Continuation of bootstrap template 2015.05

--- application/cars.schmolck.de/template/2015.05/scripts.js
+++ application/cars.schmolck.de/template/2015.05/scripts.js
@@ -1,16 +1,9 @@
-$(document).ready(function() {		
-	/*
-	 * NAVIGATION
-	 */
-	$('.menu .helper').click(function() {
-		$('.menu .helper').hide();
-		$('.menu .list').slideDown('fast');
-		return false;
-	});
-	
-	/*
-	 * ANIMATION
-	 */
-	$('.slideDown').slideDown('slow');
-	
+$(document).ready(function () {
+   /*
+    * ANIMATIONS
+    */
+   setTimeout(function () {
+      $('.alert').slideUp('slow');
+   }, 10000);
+
 });
