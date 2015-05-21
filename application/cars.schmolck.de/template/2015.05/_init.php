1694a09a23d7cac18ebcb079598fa9d3c01c5a0d
Pascal Schmolck <mail@pascalschmolck.de>
2015-05-21 09:33:16 +0200

    Begin bootstrap template 2015.05
    Begin of building a bootstrap based template within 2015.05 folder

---
+++ application/cars.schmolck.de/template/2015.05/_init.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * SCRIPTS
+ */
+$objCore = Schmolck_Framework_Core::getInstance($this);
+$strTemplatePath = $objCore->getHelperApplication()->getTemplatePath();
+// - foreign scripts
+//$objCore->registerLayoutScript('library/jquery/jquery-1.9.1.min.js');
+$objCore->registerLayoutScript('library/bower_components/jquery/dist/jquery.min.js');
+$objCore->registerLayoutScript('library/bower_components/bootstrap/dist/js/bootstrap.min.js');
+// - framework scripts
+$objCore->registerLayoutScript('library/schmolck/framework/helper/api/api.js');
+$objCore->registerLayoutScript('library/schmolck/framework/helper/link/link.js');
+// - layout scripts
+$objCore->registerLayoutScript($strTemplatePath . '/scripts.js');
+
+/*
+ * STYLES
+ */
+$objCore->registerLayoutStyle('library/bower_components/bootstrap/dist/css/bootstrap.min.css');
+//$objCore->registerLayoutStyle($strTemplatePath . '/styles.default.less');
+$objCore->registerLayoutStyle($strTemplatePath . '/styles.less');
+
+/*
+ * TITLE
+ */
+if ($objCore->getHelperHtml()->getPageTitle() != '') {
+	$strPageTitle = $objCore->getHelperHtml()->getPageTitle() . ' | ';
+}
