772677eb6a4bb8447e7e70b6c9ebed3fc2e7130e
Pascal Schmolck <mail@pascalschmolck.de>
2015-05-31 00:26:58 +0200

    Add new method to JSON api
    Add 'getCarsFiltered' to JSON api

--- application/cars.schmolck.de/module/mobile/api/json/action.php
+++ application/cars.schmolck.de/module/mobile/api/json/action.php
@@ -11,33 +11,35 @@ $strParameterFunction = $_GET['function'];
  * OUTPUT
  */
 try {
-   switch ($strParameterFunction) {
-      // mobile/api/json/function/getFilterOptions
-      case 'getFilterOptions':
-         $arrBrands = $objCars->getFilterOptionsBrands();
-         foreach ($arrBrands as $key => $value) {
-            $arrResult['brand'][$key] = array(
-               'label' => $value,
-               'models' => $objCars->getFilterOptionsModels($key)
-            );
-         }
-         break;
-   } 
+    switch ($strParameterFunction) {
+        case 'getFilterOptions':
+            $arrBrands = $objCars->getFilterOptionsBrands();
+            foreach ($arrBrands as $key => $value) {
+                $arrResult['brand'][$key] = array(
+                    'label' => $value,
+                    'models' => $objCars->getFilterOptionsModels($key)
+                );
+            }
+            break;
+        case 'getCarsFiltered':
+            $arrResult['cars'] = $objCars->queryCarsFiltered();
+            break;
+    }
 } catch (Exception $objException) {
-   $arrResult = array(
-     'status' => $objException->getCode(),
-     'result' => $objException->getMessage()
-   );
+    $arrResult = array(
+        'status' => $objException->getCode(),
+        'result' => $objException->getMessage()
+    );
 }
 
 /*
  * OUTPUT
  */
 if (!empty($strParameterFunction)) {
-   $strJSON = json_encode($arrResult);
-   header("Content-Type: application/json; charset=utf-8");
-   header("Content-Transfer-Encoding: 8bit");
-   header("Content-Length: ".strlen($strJSON));
-   echo $strJSON;
-   exit();
+    $strJSON = json_encode($arrResult);
+    header("Content-Type: application/json; charset=utf-8");
+    header("Content-Transfer-Encoding: 8bit");
+    header("Content-Length: " . strlen($strJSON));
+    echo $strJSON;
+    exit();
 }
