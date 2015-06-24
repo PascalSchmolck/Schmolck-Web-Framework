3ff791c41771333cd6ad1fcbee0960d5ee0602be
Pascal Schmolck <mail@pascalschmolck.de>
2015-06-25 00:42:03 +0200

    Fix IE issues
    Thankfully I found all the necessary infos on:
    http://www.telegraphicsinc.com/2013/10/using-bootstrap-3-with-internet-explorer-8-ie8/

--- application/cars.schmolck.de/template/2015.06/_init.php
+++ application/cars.schmolck.de/template/2015.06/_init.php
@@ -8,7 +8,10 @@ $strTemplatePath = $objCore->getHelperApplication()->getTemplatePath();
 /*
  * SCRIPTS
  */
-// - foreign
+// - foreign (IE hacks)
+$objCore->registerLayoutScript('library/bower_components/css3-mediaqueries-js/css3-mediaqueries.js');
+$objCore->registerLayoutScript('library/bower_components/html5shiv/dist/html5shiv.min.js');
+// - foreign (regular scripts)
 $objCore->registerLayoutScript('library/bower_components/jquery/dist/jquery.min.js');
 $objCore->registerLayoutScript('library/bower_components/bootstrap/dist/js/bootstrap.min.js');
 // - layout
@@ -19,7 +22,7 @@ $objCore->registerLayoutScript($strTemplatePath . '/scripts.js');
  */
 $objCore->registerLayoutStyle('library/bower_components/bootstrap/dist/css/bootstrap.min.css');
 //$objCore->registerLayoutStyle('library/bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
-$objCore->registerLayoutStyle('library/bower_components/bootstrap/themes/cyborg/bootstrap.min.css');
+$objCore->registerLayoutStyle('library/css/bootstrap/themes/cyborg/bootstrap.min.css');
 $objCore->registerLayoutStyle($strTemplatePath . '/styles.less');
 
 /*
