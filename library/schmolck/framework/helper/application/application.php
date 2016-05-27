8aefbb9665128fd970a79772b100d1d884e7182c
Pascal Schmolck <mail@pascalschmolck.de>
2016-05-27 17:53:58 +0200

    Enable working framework on "non-vhost" environments
    Special fixes for making the framework "non-vhost" environment
    compatible

--- library/schmolck/framework/helper/application/application.php
+++ library/schmolck/framework/helper/application/application.php
@@ -8,124 +8,126 @@
  */
 class Schmolck_Framework_Helper_Application extends Schmolck_Framework_Helper {
 
-	const PATH = 'application';
+    const PATH = 'application';
 
-	/**
-	 * Get current application name
-	 * 
-	 * @return string lower case name
-	 */
-	public function getName() {
-		return APPLICATION_NAME;
-	}
+    /**
+     * Get current application name
+     * 
+     * @return string lower case name
+     */
+    public function getName() {
+        return APPLICATION_NAME;
+    }
 
-	/**
-	 * Get current application path
-	 * 
-	 * @return string lower case host path
-	 */
-	public function getPath() {
-		return self::PATH . '/' . $this->getName();
-	}
-	
-	/**
-	 * Get current module path
-	 * 
-	 * @return string module path
-	 */
-	public function getModulePath() {
-		return $this->getPath() . '/module';
-	}
-   
-   /**
-    * Get current action path
-    * 
-    * @return string action path
-    */
-   public function getActionPath() {
-      return $this->getModulePath() . '/' . $this->getRequestUri();
-   }
+    /**
+     * Get current application path
+     * 
+     * @return string lower case host path
+     */
+    public function getPath() {
+        return self::PATH . '/' . $this->getName();
+    }
 
-	/**
-	 * Get current template path
-	 * 
-	 * @return string template path
-	 */
-	public function getTemplatePath() {
-		return $this->getPath() . '/template/' . APPLICATION_TEMPLATE;
-	}
+    /**
+     * Get current module path
+     * 
+     * @return string module path
+     */
+    public function getModulePath() {
+        return $this->getPath() . '/module';
+    }
 
-	/**
-	 * Get base URL
-	 * 
-	 * e.g. http://localhost/project/schmolck/framework/
-	 * 
-	 * @return string base URL
-	 */
-	public function getBaseUrl() {
-		$strHost = $_SERVER['HTTP_HOST'];
-		$strPath = dirname($_SERVER['PHP_SELF']);
-		if(substr($strPath, 0, 1) == "/" or substr($strPath, 0, 1) == "\\"){
-			$strPath = substr($strPath, 1, strlen($strPath));
-		}
-		return "http://{$strHost}{$strPath}";
-	}
+    /**
+     * Get current action path
+     * 
+     * @return string action path
+     */
+    public function getActionPath() {
+        return $this->getModulePath() . '/' . $this->getRequestUri();
+    }
 
-	/**
-	 * Get request url
-	 * 
-	 * @return string
-	 */
-	public function getRequestUrl() {
-		/*
-		 * PREPARATION
-		 */
-		// - http://host/module/controller/action		
-		$strHost = $_SERVER['HTTP_HOST'];
-		$strUri = $this->getRequestUri();
-		
-		/*
-		 * OUTPUT
-		 */
-		return 'http://'.$strHost.'/'.$strUri;		
-	}
-	
-	/**
-	 * Get request uri
-	 * 
-	 * @return string
-	 */
-	public function getRequestUri() {
-		/*
-		 * INITIALISATION
-		 */
-		$objCore = Schmolck_Framework_Core::getInstance($this->_objCore);
+    /**
+     * Get current template path
+     * 
+     * @return string template path
+     */
+    public function getTemplatePath() {
+        return $this->getPath() . '/template/' . APPLICATION_TEMPLATE;
+    }
 
-		/*
-		 * OUTPUT
-		 */
-		return $objCore->getModule() . '/' . $objCore->getController() . '/' . $objCore->getAction();
-	}
-	
-	/**
-	 * Get current parameter
-	 * 
-	 * @return string
-	 */
-	public function getCurrentParameter() {
-		// - module/controller/action
-		return substr($_SERVER['REQUEST_URI'], 1, strlen($_SERVER['REQUEST_URI']));
-	}
-    
-	/**
-	 * Get current uri
+    /**
+     * Get base URL
+     * 
+     * e.g. http://localhost/project/schmolck/framework/
+     * 
+     * @return string base URL
+     */
+    public function getBaseUrl() {
+        $strHost = $_SERVER['HTTP_HOST'];
+        $strPath = dirname($_SERVER['PHP_SELF']);
+        if ($strHost != 'localhost') {
+            if (substr($strPath, 0, 1) == "/" or substr($strPath, 0, 1) == "\\") {
+                $strPath = substr($strPath, 1, strlen($strPath));
+            }
+        }
+        return "http://{$strHost}{$strPath}";
+    }
+
+    /**
+     * Get request url
+     * 
+     * @return string
+     */
+    public function getRequestUrl() {
+        /*
+         * PREPARATION
+         */
+        // - http://host/module/controller/action		
+        $strHost = $_SERVER['HTTP_HOST'];
+        $strUri = $this->getRequestUri();
+
+        /*
+         * OUTPUT
+         */
+        return 'http://' . $strHost . '/' . $strUri;
+    }
+
+    /**
+     * Get request uri
+     * 
+     * @return string
+     */
+    public function getRequestUri() {
+        /*
+         * INITIALISATION
+         */
+        $objCore = Schmolck_Framework_Core::getInstance($this->_objCore);
+
+        /*
+         * OUTPUT
+         */
+        return $objCore->getModule() . '/' . $objCore->getController() . '/' . $objCore->getAction();
+    }
+
+    /**
+     * Get current parameter
+     * 
+     * @return string
+     */
+    public function getCurrentParameter() {
+        // - module/controller/action
+        return substr($_SERVER['REQUEST_URI'], 1, strlen($_SERVER['REQUEST_URI']));
+    }
+
+    /**
+     * Get current uri
      * 
      * e.g. http://server.domain/module/controller/action
-	 * 
-	 * @return string
-	 */
-	public function getCurrentUri() {
-		return $this->getBaseUrl() . '/' . $this->getCurrentParameter();
-	}	    
+     * 
+     * @return string
+     */
+    public function getCurrentUri() {
+        return $this->getBaseUrl() . '/' . $this->getCurrentParameter();
+    }
 
 }
