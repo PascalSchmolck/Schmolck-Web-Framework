29893149289b4bc45c3e71bb8cac6dcde61f20d1
Pascal Schmolck <mail@pascalschmolck.de>
2017-02-24 16:42:14 +0100

    Move menu renedering into layout file
    Move menu rendering from particular pages to global layout file

--- application/framework.schmolck.de/module/index/_init.php
+++ application/framework.schmolck.de/module/index/_init.php
@@ -6,6 +6,19 @@
 $objCore = Schmolck_Framework_Core::getInstance($this);
 
 /*
+ * MENU
+ */
+$objCore->menu = array(
+    array(
+        'link' => 'index/index/demo',
+        'label' => $objCore->getHelperTranslator()->_("Active"),
+    ),
+    array(
+        'link' => 'index',
+        'label' => $objCore->getHelperTranslator()->_("Inactive"),
+    ),
+);
+/*
  * ROUTE
  */
 if ($objCore->getAction() == 'index') {
