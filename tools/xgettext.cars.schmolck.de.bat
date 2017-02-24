b367074f978bf7989e74b185e654b06716b218bf
Pascal Schmolck <mail@pascalschmolck.de>
2017-02-24 17:04:09 +0100

    Translate current status
    New translation into german

--- tools/xgettext.cars.schmolck.de.bat
+++ tools/xgettext.cars.schmolck.de.bat
@@ -1,1 +1,2 @@
 php xgettext.php ../application/cars.schmolck.de/ > ../application/cars.schmolck.de/translation/en.pot
+php xgettext.php ../application/framework.schmolck.de/ > ../application/framework.schmolck.de/translation/en.pot
